917bf4283f42e3a613d5403a76a4721fe218b535
Violet Cat <legomatthew39@gmail.com>
2026-07-09 16:36:55 -0400

    Building the framework for the password checker program

---
+++ challenge_one/password_checker/password_checker.py
@@ -0,0 +1,62 @@
+""" Password Strength Checker 
+    Checks to see the strength of a given password dependent on multiple parameters """
+
+from string_parser import *
+
+ASCII_UPPER_MAX = 90
+ASCII_UPPER_MIN = 65
+ASCII_LOWER_MAX = 122
+ASCII_LOWER_MIN = 97
+
+def get_array(password):
+    """
+    Uses the helper method to parse the string into an array
+    """
+    return parse_string(password)
+
+def has_uppercase(password):
+    """
+    Checks the parsed password for an uppercase character
+    """
+
+def has_lowercase(password):
+    """
+    Checks the parsed password for an lowercase character
+    """
+
+def has_numeric(password):
+    """
+    Checks the parsed password to see if it has a number
+    """
+
+def has_special(password):
+    """
+    Checks to see if the password has a special character
+    """
+
+def recurrence_limiter(password):
+    """
+    Checks to see if the password has 3 or more of the same character in a row
+    """
+
+def size_checker(password):
+    """
+    Checks to see if the password is in an acceptable size range (8 to 32)
+    """
+
+def above_ten_characters(password):
+    """
+    Checks to see if the password is above ten characters
+    """
+
+def main():
+    """
+    Main Function
+    """
+    print("Error! Code not yet complete!")
+
+if __name__ == "__main__":
+    """
+    Runguard
+    """
+    main()
