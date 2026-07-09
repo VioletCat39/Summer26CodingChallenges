1ea9e0c0cd625ef4a2e57659497fc0591d24491d
Violet Cat <legomatthew39@gmail.com>
2026-07-09 16:30:19 -0400

    Helper function to parse the string into an array of characters

---
+++ challenge_one/password_checker/string_parser.py
@@ -0,0 +1,25 @@
+""" String Parser by Violet Cat 39 """
+""" A Helper function/class to parse each character in a string """
+
+def parse_string(line):
+    """
+    Parses the String into a character array
+    """
+    character_list = []
+    for item in line:
+        character_list.append(item)
+    return character_list;
+
+def main():
+    """
+    Main Function for manual testing
+    """
+    line = "Hello Wolrd!"
+    print(parse_string(line))
+
+if __name__ == "__main__":
+    """
+    Runguard
+    """
+    main()
+
