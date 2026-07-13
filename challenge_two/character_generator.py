e65a3b720a9d8ee9dd5d9b88bbd13ca682d34b74
Violet Cat <legomatthew39@gmail.com>
2026-07-13 17:54:51 -0400

    This code generates a random character for the password generator!

---
+++ challenge_two/character_generator.py
@@ -0,0 +1,25 @@
+""" Random Character Generator by Violet Cat 39 """
+
+import string
+import random
+
+ACCEPTABLE_CHARS = list(string.ascii_letters + string.digits + string.punctuation)
+
+def get_random_char():
+    index = random.randrange(0, len(ACCEPTABLE_CHARS))
+    return ACCEPTABLE_CHARS[index]
+
+def main():
+    """
+    Main function to test elements of code manually
+    """
+    print(ACCEPTABLE_CHARS)
+    for i in range(10):
+        print(get_random_char())
+
+
+if __name__ == "__main__":
+    """
+    Runguard (As recommended by local Soft Dev Professors near you!)
+    """
+    main()
